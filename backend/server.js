181fc0915ea3214d5ec2b973aa8919c3bd68b6d6
kristofvad <kristof.vad@gmail.com>
2022-09-15 22:46:14 +0200

    starting user requests

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/sessions', require('./routes/sessionRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
